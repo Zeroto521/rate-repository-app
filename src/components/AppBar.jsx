1a146b41bceb49a56d18482b2ab12e593a818e2b
Zero <Zeroto521@gmail.com>
2021-02-03 10:07:28 +0800

    use react-native-paper package

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Appbar } from 'react-native-paper';
+
+
+const AppBar = () => {
+  return (
+  <Appbar.Header>
+    <Appbar.Content title="Repositories" />
+  </Appbar.Header>
+  );
+};
+
+export default AppBar;
