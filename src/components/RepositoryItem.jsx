6b75b805a9bb077b47b5e6ef81aca6d4abd32b63
Zero <Zeroto521@gmail.com>
2021-02-04 10:04:16 +0800

    :tada: finish 10.4-10.5

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Avatar, Card, Button, DataTable } from 'react-native-paper';
 
-const RepositoryItem = ({ item }) => (
-  <View>
-    <Text>Full Name: {item.fullName}</Text>
-    <Text>Description: {item.description}</Text>
-    <Text>Language: {item.language}</Text>
-    <Text>Stars: {item.stargazersCount}</Text>
-    <Text>Forks: {item.forksCount}</Text>
-    <Text>Reviews: {item.reviewCount}</Text>
-    <Text>Rating: {item.ratingAverage}</Text>
-  </View>
-);
+
+const NumberShowing = ({ number }) => {
+  if (number > 1000)
+    return `${(number / 1000).toFixed(1)}k`;
+  else
+    return number;
+};
+
+
+const RepositoryItem = ({ item }) => {
+
+  const LeftContent = () => <Avatar.Image size={50} source={item.ownerAvatarUrl} />;
+
+  return (
+    <Card>
+      <Card.Title title={item.fullName} subtitle={item.description} subtitleNumberOfLines={2} left={LeftContent} />
+      <Card.Content>
+        <Button mode="contained" >{item.language}</Button>
+        <DataTable>
+          <DataTable.Row>
+            <DataTable.Cell><NumberShowing number={item.stargazersCount} /></DataTable.Cell>
+            <DataTable.Cell><NumberShowing number={item.forksCount} /></DataTable.Cell>
+            <DataTable.Cell><NumberShowing number={item.reviewCount} /></DataTable.Cell>
+            <DataTable.Cell><NumberShowing number={item.ratingAverage} /></DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Header>
+            <DataTable.Title>Stars</DataTable.Title>
+            <DataTable.Title>Forks</DataTable.Title>
+            <DataTable.Title>Reviews</DataTable.Title>
+            <DataTable.Title>Rating</DataTable.Title>
+          </DataTable.Header>
+        </DataTable>
+      </Card.Content>
+    </Card>
+  );
+};
 
 export default RepositoryItem;
