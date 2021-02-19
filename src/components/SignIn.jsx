44b30b345ba8de9a521cba2b250e2c0861d12c8f
Zero <Zeroto521@gmail.com>
2021-02-19 14:49:19 +0800

    finish 10.8

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,60 @@
+import { Formik } from 'formik';
 import React from 'react';
+import { useHistory } from 'react-router-native';
+import * as yup from 'yup';
 
-import Text from './Text';
+import { SignInForm } from './SignInForm';
+
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(3, 'Username must be at least four characters long')
+    .required('Username is required'),
+
+  password: yup
+    .string()
+    .min(6, 'Password must be at least six characters long')
+    .required('Password is required'),
+});
+
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+export const SignInContainer = ({
+  initialValues,
+  onSubmit,
+  validationSchema,
+}) => {
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
+};
 
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  const history = useHistory();
+
+  const onSubmit = async (values) => {
+    console.log(values);
+    history.push('/');
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
