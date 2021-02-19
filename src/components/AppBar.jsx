7add5b65725d2a6be28607fe62f7d20f494cb453
Zero <Zeroto521@gmail.com>
2021-02-19 14:32:37 +0800

    finish 10.6

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,12 +1,57 @@
+import Constants from 'expo-constants';
 import React from 'react';
+import { StyleSheet, TouchableWithoutFeedback, View, ScrollView } from 'react-native';
 import { Appbar } from 'react-native-paper';
+import { Link } from 'react-router-native';
 
+import theme from '../theme';
+import Text from './Text';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: theme.colors.appBarBackground,
+  },
+  scrollView: {
+    flexDirection: 'row',
+  },
+  tabTouchable: {
+    flexGrow: 0,
+  },
+  tabContainer: {
+    paddingHorizontal: 15,
+    paddingBottom: 20,
+    paddingTop: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tabText: {
+    fontSize: theme.fontSizes.subheading,
+    color: 'white',
+  },
+});
+
+const AppBarTab = ({ children, ...props }) => {
+  return (
+    <TouchableWithoutFeedback style={styles.tabTouchable} {...props}>
+      <View style={styles.tabContainer}>
+        <Text fontWeight="bold" style={styles.tabText}>
+          {children}
+        </Text>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
 
 const AppBar = () => {
   return (
-  <Appbar.Header>
-    <Appbar.Content title="Repositories" />
-  </Appbar.Header>
+    <Appbar.Header>
+      <ScrollView style={styles.scrollView} horizontal>
+        <Link to="/" component={AppBarTab}>Repositories</Link>
+        <Link to="/signin" component={AppBarTab}>Sign in</Link>
+      </ScrollView>
+    </Appbar.Header>
   );
 };
 
