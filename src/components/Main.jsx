e9325d50be6c1066df20ee4db2437758b77e16d3
Zero <Zeroto521@gmail.com>
2021-02-19 14:32:37 +0800

    finish 10.6-10.7

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Redirect, Route, Switch } from 'react-router-native';
 
 import AppBar from './AppBar';
 import RepositoryList from './RepositoryList';
+import SignIn from './SignIn';
+
 
 const Main = () => {
   return (
     <View>
       <AppBar />
-      <RepositoryList />
+      <Switch>
+        <Route path="/signin">
+          <SignIn />
+        </Route>
+        <Route path="/">
+          <RepositoryList />
+        </Route>
+        <Redirect to="/signin" />
+      </Switch>
     </View>
   );
 };
